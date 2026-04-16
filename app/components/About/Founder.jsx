5cf50976ab768ba8b9e8c0a2e8e915ab4d68a334
shahabhusain <shahabhussain098123@gmail.com>
2026-04-16 17:53:50 +0500

    chnages

--- app/components/About/Founder.jsx
+++ app/components/About/Founder.jsx
@@ -3,7 +3,7 @@ import { useLocale } from 'next-intl'
 import bg from '@/public/about.svg'
 import founder from '@/public/founder.svg'
 import Image from 'next/image'
-import { FaInstagram, FaTwitter, FaYoutube } from 'react-icons/fa'
+import { FaInstagram, FaLinkedin, FaTwitter, FaYoutube } from 'react-icons/fa'
 
 const Founder = () => {
     const locale = useLocale()
@@ -18,8 +18,8 @@ const Founder = () => {
         : {
             name: "Samin Jan",
             title: "Founder & CEO",
-            description1: "As an artist here to solve such issues, we successfully filed several mandamus cases on behalf of more than 250 artwork and their family members and had approved green cards within several months after filing a complaint.",
-            description2: "As an artist here to solve such issues, we successfully filed several mandamus cases on behalf of more than 250 artwork and their family members and had approved green cards within several months after filing a complaint."
+            description1: "Our journey at 800BatteryPro started from a vision of delivering 100 percent customer satisfactory and premium car battery services. When it started, we only had 3 technicians and 1 mobile van for roadside assistance; now we are aiming to become one of the leading 24/7 roadside assistance providers in the UAE. With 100+ certified technicians and 20+ Mobile Vans, we provide onsite car battery replacement and boosting services within 30 minutes anywhere in Dubai and Abu Dhabi. We want to offer a complete and international standard of car repair workshop. Our certified technicians are well-experienced in working on all major car brands, including the top American & European car makes. Our technicians are trained to use only OEM-approved spare parts and only install genuine car batteries for your car. There is no hidden cost while we offer Free Battery Testing along with Free Car Inspection to ensure your safety on the road.",
+            description2: "Don’t Panic If you Ever Have a Dead Battery! Just give 800BatteryPro a Call and we will ensure you get back on the road asap."
         }
 
     return (
@@ -33,15 +33,9 @@ const Founder = () => {
                     <h1 className='text-red-800 text-[40px] font-semibold'>{text.name}</h1>
                     <h2 className='text-[20px] font-semibold'>{text.title}</h2>
                     <div className='flex items-center gap-3'>
-                        <span className='bg-[#f8f8f8] p-3 rounded-full cursor-pointer hover:bg-red-600 hover:text-white transition-colors'>
-                            <FaYoutube />
-                        </span>
-                        <span className='bg-[#f8f8f8] p-3 rounded-full cursor-pointer hover:bg-red-600 hover:text-white transition-colors'>
-                            <FaInstagram />
-                        </span>
-                        <span className='bg-[#f8f8f8] p-3 rounded-full cursor-pointer hover:bg-red-600 hover:text-white transition-colors'>
-                            <FaTwitter />
-                        </span>
+                        <a href='https://www.linkedin.com/in/samin-jan-51a280342/' target='_blank' className='bg-[#f8f8f8] p-3 rounded-full cursor-pointer hover:bg-red-600 hover:text-white transition-colors'>
+                            <FaLinkedin />
+                        </a>
                     </div>
                     <p className='text-[#00000099] mt-6'>{text.description1}</p>
                     <p className='text-[#00000099] mt-4'>{text.description2}</p>
